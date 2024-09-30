c7d06e4812edab1437f77df7b1c86dfbed1cb477
emanuele23dev <emanuele.meggiarin@libero.it>
2024-09-30 14:08:32 +0200

    seconda fase raccolta dati

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ let numero_km;
 // creo la seconda variabile dove chiedere all'utente l'età 
 let eta;
 
+// definizione prezzo biglietto in base ai km
+let price_km = 0.21;
+
+// seconda fase : raccogliamo i dati
+// utilizzo un prompt per chiedere e salvare i km utente
+numero_km = prompt("Ciao, quanti km devi percorrere?");
+// utilizzo un prompt per chiedere e salvare età 
+eta = prompt("Ciao, quanti anni hai?");
+console.log(numero_km);
+console.log(eta);
